e2f377b0958f69fbd8466eca2036e73afee517ca
Vivek Kumar Kanaujia <vivek.aris@gmail.com>
2016-07-05 13:52:37 +0530

    Version1
    Basic Pojo

---
+++ build/web/add.jsp
@@ -0,0 +1,60 @@
+<%-- 
+    Document   : add
+    Created on : 4 Jul, 2016, 6:32:31 PM
+    Author     : Vivek
+--%>
+
+<%@page import="com.somendra.hibernate.Employee"%>
+<%@page import="org.hibernate.Session"%>
+<%@page import="org.hibernate.SessionFactory"%>
+<%@page import="com.somendra.hibernate.HibernateUtil"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <%  
+            
+            //geting value from form
+          int id=Integer.parseInt(request.getParameter("id"));
+         String name=(String)request.getParameter("name");
+          int salary=Integer.parseInt(request.getParameter("salary"));
+
+          //checking the input value
+           /* out.println("Id is "+id);
+            out.println("name is "+name);
+            out.println("salary is "+salary);
+            */
+           
+         //create object of model/pojo class  
+         Employee emp=new Employee();
+         //set all required value
+         emp.setEmpId(id);
+         emp.setName(name);
+         emp.setSalary(salary);
+         
+//hibernateing.............started
+        //step1
+SessionFactory sessionFactory=HibernateUtil.getSessionFactory();
+//step2
+Session hsession = sessionFactory.openSession();
+//step3
+hsession.beginTransaction();
+//step4
+// this would save the table object into the database
+hsession.save(emp);
+//step5
+hsession.getTransaction().commit();
+//step6
+//hsession.close();
+//step7
+//sessionFactory.close();
+        
+        %>
+        
+    </body>
+</html>
---
+++ src/java/com/somendra/hibernate/Employee.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.somendra.hibernate;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Vivek
+ */
+@Entity
+@Table(name = "employee")
+@NamedQueries({
+    @NamedQuery(name = "Employee.findAll", query = "SELECT e FROM Employee e")})
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "emp_id")
+    private Integer empId;
+    @Size(max = 30)
+    @Column(name = "name")
+    private String name;
+    @Column(name = "salary")
+    private Integer salary;
+
+    public Employee() {
+    }
+
+    public Employee(Integer empId) {
+        this.empId = empId;
+    }
+
+    public Integer getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(Integer empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (empId != null ? empId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Employee)) {
+            return false;
+        }
+        Employee other = (Employee) object;
+        if ((this.empId == null && other.empId != null) || (this.empId != null && !this.empId.equals(other.empId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.somendra.hibernate.Employee[ empId=" + empId + " ]";
+    }
+    
+}
